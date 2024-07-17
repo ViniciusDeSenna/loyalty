84114fb18ded458f4bb6fbc64e5b4e4c4f16e2f2
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-07-17 17:32:53 -0300

    biblioteca qrcode

--- resources/views/modules/cartoes/index.blade.php
+++ resources/views/modules/cartoes/index.blade.php
@@ -1,58 +1,46 @@
 @extends('dashboard')
 
 @section('content')
-<!-- Para cada cartao que o usuário possuir: -->
-<div class="card">
-    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/1e/8b/16/oliv-restaurante.jpg?w=600&h=400&s=1" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Oliv</h5>
-      <p class="card-text">Você tem 4 pontos de fidelidade!</p>
-      <a id="show-image-btn" href="#" class="btn btn-primary">Marcar um ponto!</a>
-    </div>
-    <div id="card-qrcode" class="card-footer d-none">
-        
-    </div>
-</div>
+<script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
 
-  <script>
-    document.getElementById('show-image-btn').addEventListener('click', function(e) {
-      e.preventDefault();
-      $('#card-qrcode').slideDown();
-      $('#card-qrcode').removeClass('d-none');
-    });
+<div class="form">
+  <h1>QR Code using qrcodejs</h1>
+  <form>
+    <input type="url" id="website" name="website" placeholder="https://webisora.com" required />
+    <button type="button" onclick="generateQRCode()">
+      Generate QR Code
+    </button>
+  </form>
+
+  <div id="qrcode-container">
+    <div id="qrcode" class="qrcode"></div>
+    <h4>With some styles</h4>
+    <div id="qrcode-2" class="qrcode"></div>
+  </div>
+
+  <script type="text/javascript">
+    function generateQRCode() {
+      let website = document.getElementById("website").value;
+      if (website) {
+        let qrcodeContainer = document.getElementById("qrcode");
+        qrcodeContainer.innerHTML = "";
+        new QRCode(qrcodeContainer, website);
+        /*With some styles*/
+        let qrcodeContainer2 = document.getElementById("qrcode-2");
+        qrcodeContainer2.innerHTML = "";
+        new QRCode(qrcodeContainer2, {
+          text: website,
+          width: 128,
+          height: 128,
+          colorDark: "#5868bf",
+          colorLight: "#ffffff",
+          correctLevel: QRCode.CorrectLevel.H
+        });
+        document.getElementById("qrcode-container").style.display = "block";
+      } else {
+        alert("Please enter a valid URL");
+      }
+    }
   </script>
-
-<!-- <main>
-    <div id="reader"></div>
-    <div id="result"></div>
-</main>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.4/html5-qrcode.min.js" integrity="sha512-k/KAe4Yff9EUdYI5/IAHlwUswqeipP+Cp5qnrsUjTPCgl51La2/JhyyjNciztD7mWNKLSXci48m7cctATKfLlQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script>
-    const scanner = new Html5QrcodeScanner('reader', { 
-        qrbox: {
-            width: 250,
-            height: 250,
-        },  
-        fps: 20,
-    });
-    scanner.render(success, error);
-    function success(result) {
-
-        document.getElementById('result').innerHTML = `
-        <h2>Success!</h2>
-        <p><a href="${result}">${result}</a></p>
-        `;
-
-        scanner.clear();
-
-        document.getElementById('reader').remove();    
-    }
-    function error(err) {
-        console.error(err);
-    }
-
-    $(function(){
-        $('#html5-qrcode-button-camera-permission')
-    });
-</script> -->
+</div>
 @endsection
