164422e9659e6478c9279821e1a36568ac89dc39
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-09-01 16:42:58 -0300

    ajuste assets

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,10 +1,10 @@
 <!-- JQuery -->
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <!--   Core JS Files   -->
-<script src="{{asset('tema/assets/assets/js/core/popper.min.js')}}"></script>
-<script src="{{asset('tema/assets/assets/js/core/bootstrap.min.js')}}"></script>
-<script src="{{asset('tema/assets/assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
-<script src="{{asset('tema/assets/assets/js/plugins/smooth-scrollbar.min.js')}}"></script>
-<script src="{{asset('tema/assets/assets/js/plugins/chartjs.min.js')}}"></script>
+<script src="{{asset('tema/assets/js/core/popper.min.js')}}"></script>
+<script src="{{asset('tema/assets/js/core/bootstrap.min.js')}}"></script>
+<script src="{{asset('tema/assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
+<script src="{{asset('tema/assets/js/plugins/smooth-scrollbar.min.js')}}"></script>
+<script src="{{asset('tema/assets/js/plugins/chartjs.min.js')}}"></script>
 <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc (abre o menu lateral) -->
-<script src="{{asset('tema/assets/js/soft-ui-dashboard.min.js?v=1.0.7')}}"></script>
+<script src="{{asset('tema/js/soft-ui-dashboard.min.js?v=1.0.7')}}"></script>
